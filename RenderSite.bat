80871f7ef61d5b9b759a6b0cb0e4c12257bb1631
huntercole25 <53839906+huntercole25@users.noreply.github.com>
2024-05-24 12:16:45 -0400

    Updating for terminal-based html generation

---
+++ RenderSite.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" -e "library(rmarkdown);Sys.setenv(RSTUDIO_PANDOC='C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools/');rmarkdown::render('C:/Users/HunterCole/Documents/ENSO_Report_SCI/ENSO_Report.Rmd', output_file = 'C:/Users/HunterCole/Documents/ENSO_Report_SCI/index.html')
