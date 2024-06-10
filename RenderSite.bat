961bac71f34d0087e84f35c9beaab0dc9e313cf0
huntercole25 <53839906+huntercole25@users.noreply.github.com>
2024-06-10 16:55:23 -0400

    Update RenderSite.bat

--- RenderSite.bat
+++ RenderSite.bat
@@ -1,1 +1,9 @@
 "C:\Program Files\R\R-4.2.2\bin\Rscript.exe" -e "library(rmarkdown);Sys.setenv(RSTUDIO_PANDOC='C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools/');rmarkdown::render('C:/Users/HunterCole/Documents/ENSO_Report_SCI/ENSO_Report.Rmd', output_file = 'C:/Users/HunterCole/Documents/ENSO_Report_SCI/index.html')
+
+git -C "C:\Users\HunterCole\Documents\ENSO_Report_SCI" fetch
+
+git -C "C:\Users\HunterCole\Documents\ENSO_Report_SCI" pull
+
+git -C "C:\Users\HunterCole\Documents\ENSO_Report_SCI" commit -m "Site update" ".\index.html"
+
+git -C "C:\Users\HunterCole\Documents\ENSO_Report_SCI" push
